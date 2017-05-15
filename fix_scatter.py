69b0952fd67f83946ace8b1c199769f2888eb000
Andrey Shedko <sirius.frozen@gmail.com>
2017-05-15 08:46:37 +0300

    New version with new caching through pickle

--- fix_scatter.py
+++ fix_scatter.py
@@ -10,7 +10,6 @@ import matplotlib.pyplot as plt
 import scipy.stats as stats
 from sklearn.linear_model import BayesianRidge
 
-
 column_names = ['CURRENT_SAC_END_TIME', 'CURRENT_SAC_AVG_VELOCITY', 'CURRENT_SAC_PEAK_VELOCITY', 'CURRENT_SAC_DURATION']
 column_names_fix = ['CURRENT_FIX_END', 'CURRENT_FIX_DURATION']
 column_dict = {}
@@ -18,73 +17,80 @@ skip_val = ['.']
 
 step = 30000
 
-Health = ["Без заикания",  "Во время лечения","До лечения"]
+Health = ["Без заикания", "Во время лечения", "До лечения"]
+
 
 def dists(v):
     mean = np.mean(v)
     return np.asarray([np.linalg.norm(x - mean) for x in v])
 
-class EyeDat:
 
+class EyeDat:
     """ The class that handles the dataset """
+
     def __init__(self, health, exp, trial):
         self.health = health
         self.exp = exp
         patients = os.listdir("./Данные айтрекера/{}/".format(health))
-        fnames = [(glob.glob("./*/{}/{}/Fix*_{}*".format(health, pat, exp))[0], glob.glob("./*/{}/{}/Simple*_{}*".format(health, pat, exp))[0]) for pat in patients]
-        df_s = [(pd.read_csv(n[0], delim_whitespace =1, decimal=',',usecols=["CURRENT_FIX_START","CURRENT_FIX_END","CURRENT_FIX_X","CURRENT_FIX_Y","CURRENT_FIX_DURATION"]),
-                 pd.read_csv(n[1], delimiter = '\t', decimal=',',usecols=["LEFT_GAZE_X","LEFT_GAZE_Y","RIGHT_GAZE_X","RIGHT_GAZE_Y"])) for n in fnames]
+        fnames = [(glob.glob("./*/{}/{}/Fix*_{}*".format(health, pat, exp))[0],
+                   glob.glob("./*/{}/{}/Simple*_{}*".format(health, pat, exp))[0]) for pat in patients]
+        df_s = [(pd.read_csv(n[0], delim_whitespace=1, decimal=',',
+                             usecols=["CURRENT_FIX_START", "CURRENT_FIX_END", "CURRENT_FIX_X", "CURRENT_FIX_Y",
+                                      "CURRENT_FIX_DURATION"]),
+                 pd.read_csv(n[1], delimiter='\t', decimal=',',
+                             usecols=["LEFT_GAZE_X", "LEFT_GAZE_Y", "RIGHT_GAZE_X", "RIGHT_GAZE_Y"])) for n in fnames]
         for i in range(len(df_s)):
             df = df_s[i][0]
-            df = df[df["CURRENT_FIX_END"]//step % 3 == trial]
-            df_s[i] = df,df_s[i][1]
+            df = df[df["CURRENT_FIX_END"] // step % 3 == trial]
+            df_s[i] = df, df_s[i][1]
         disps = self.get_disps(df_s)
-        self.df = pd.concat([ f[0] for f in df_s ])
+        self.df = pd.concat([f[0] for f in df_s])
         self.disps = disps
 
-
-    def get_disps(self,df_s):
+    def get_disps(self, df_s):
         disps = []
-        for fixs,simp in df_s:
-            simp = simp.loc[:,(simp != '.').any(axis=0)]
-            simp = simp.stack().str.replace(',','.').unstack()
-            simp = simp[simp[simp.columns[0]]!='.'].astype(float)
-            stepsize = len(simp)/int(fixs.tail(1)["CURRENT_FIX_END"])
-            for rec in fixs[["CURRENT_FIX_START","CURRENT_FIX_END"]].itertuples():
-                fixation = np.asarray(simp[int(stepsize*rec[1]):int(stepsize*rec[2])])
+        for fixs, simp in df_s:
+            simp = simp.loc[:, (simp != '.').any(axis=0)]
+            simp = simp.stack().str.replace(',', '.').unstack()
+            simp = simp[simp[simp.columns[0]] != '.'].astype(float)
+            stepsize = len(simp) / int(fixs.tail(1)["CURRENT_FIX_END"])
+            for rec in fixs[["CURRENT_FIX_START", "CURRENT_FIX_END"]].itertuples():
+                fixation = np.asarray(simp[int(stepsize * rec[1]):int(stepsize * rec[2])])
                 disps.append(dists(fixation).std())
 
         return disps
 
-    def plot(self,axis):
+    def plot(self, axis):
         X = self.disps
         y = self.df["CURRENT_FIX_DURATION"]
-        
-        axis.scatter(X,y, s = 4)
-        axis.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
-        axis.ticklabel_format(style='sci', axis='x', scilimits=(0,0))
-        if self.exp == 1 : axis.set_ylabel(self.health)
-        if self.health == "До лечения" : axis.set_xlabel("Эксперимент "+ str(self.exp))
+
+        axis.scatter(X, y, s=4)
+        axis.ticklabel_format(style='sci', axis='y', scilimits=(0, 0))
+        axis.ticklabel_format(style='sci', axis='x', scilimits=(0, 0))
+        if self.exp == 1: axis.set_ylabel(self.health)
+        if self.health == "До лечения": axis.set_xlabel("Эксперимент " + str(self.exp))
 
         clf = BayesianRidge()
         clf.fit(np.nan_to_num(np.matrix(X)).transpose(), y)
-        L=np.linspace(1,max(X))
-        axis.plot(L,[clf.predict(x)for x in L],'r')
+        L = np.linspace(1, max(X))
+        axis.plot(L, [clf.predict(x) for x in L], 'r')
 
-        return X,y,clf
+        return X, y, clf
+
 
 def main():
     plt.tight_layout
-    f=[0,0,0]
+    f = [0, 0, 0]
     for trial in range(3):
-        f[trial], axarr = plt.subplots(3, 4,  figsize=(16, 12), dpi=160)
-        i=0
+        f[trial], axarr = plt.subplots(3, 4, figsize=(16, 12), dpi=160)
+        i = 0
         for cond in Health:
-            for exp in range(1,5):
-                dat = EyeDat(cond,exp,trial)
-                dat.plot(axarr[i,exp-1])
-            i+=1
-        plt.savefig("Trial"+ str(trial+1))
+            for exp in range(1, 5):
+                dat = EyeDat(cond, exp, trial)
+                dat.plot(axarr[i, exp - 1])
+            i += 1
+        plt.savefig("Trial" + str(trial + 1))
+
 
 if __name__ == "__main__":
     main()
--- fix_scatter_new.py
+++ fix_scatter_new.py
@@ -14,8 +14,8 @@ from sklearn.linear_model import BayesianRidge
 
 
 column_names = ["CURRENT_FIX_START","CURRENT_FIX_END","CURRENT_FIX_X","CURRENT_FIX_Y","CURRENT_FIX_DURATION"]
-# column_names_gaze = ["LEFT_GAZE_X","LEFT_GAZE_Y","RIGHT_GAZE_X","RIGHT_GAZE_Y"]
-column_names_gaze = ["LEFT_GAZE_X", "LEFT_GAZE_Y"]
+column_names_gaze = ["LEFT_GAZE_X","LEFT_GAZE_Y","RIGHT_GAZE_X","RIGHT_GAZE_Y"]
+# column_names_gaze = ["LEFT_GAZE_X", "LEFT_GAZE_Y"]
 column_dict = {}
 skip_val = ['.']
 
@@ -43,7 +43,7 @@ class EyeDat:
 
         for cond in Health:
             for exp in range(1,5): # 1,2,3,4 Эксперимент
-                patients = os.listdir("./Данные айтрекера/{}/".format(cond))
+                patients = sorted(os.listdir("./Данные айтрекера/{}/".format(cond)))
                 fnames = [(glob.glob("./*/{}/{}/Fix*_{}*".format(cond, pat, exp))[0],
                            glob.glob("./*/{}/{}/Simple*_{}*".format(cond, pat, exp))[0]) for pat in patients]
 
@@ -57,14 +57,15 @@ class EyeDat:
                     Simp = []
                     # Читаем Simp`ы
                     with open(fnames[i][1]) as csvfile:
-                        local_cols = column_names_gaze
                         reader = csv.DictReader(csvfile, delimiter='\t')
                         for tup in reader:
-                            Simp.append([tup[fld] for fld in local_cols] + [reader.line_num - 2])
+                            Simp.append([tup[fld] for fld in column_names_gaze] + [str(reader.line_num - 2)])
+
                     Fix = pd.DataFrame(Fix,columns=column_names)
                     Simp = pd.DataFrame(Simp, columns=column_names_gaze + ["IDX"])
                     Fix = Fix.applymap(locale.atof)
-                    Simp = Simp.applymap(f) # Переводим запятые в точкио
+                    Simp = Simp.loc[:, (Simp != '.').any(axis=0)]
+                    Simp = Simp.applymap(f) # Переводим запятые в точки
                     self.data["{}_{}_{}_Simp".format(cond,exp,i)] = Simp                    
                     for k in range(3):
                         self.data["{}_{}_{}_Fix_{}".format(cond,exp,i,k)] = Fix[Fix["CURRENT_FIX_END"]//step %3 ==k]
@@ -81,7 +82,7 @@ class EyeDat:
         :return: 
         """
         if type == 'Fix':
-            return self.data["{}_{}_{}_Fix_{}".format(cond,exp,pat,trial)]
+            return self.data["{}_{}_{}_Fix_{}".format(cond, exp, pat, trial)]
         elif type == 'Simp':
             return self.data["{}_{}_{}_Simp".format(cond, exp, pat)]
         else:
@@ -91,14 +92,16 @@ class EyeDat:
         disps = []
         for pat in range(self.pat_l[cond]):
             fix = self.get_df(cond,exp,pat,'Fix',trial)
-            scatter = self.get_df(cond,exp,pat,'Simp')
-            disps += self.__get_disps(fix,scatter)
+            simp = self.get_df(cond,exp,pat,'Simp')
+            disps += self.__get_disps(fix,simp)
         return disps
         
     def __get_disps(self, fixs, simp):
         disps = []
         for rec in fixs[["CURRENT_FIX_START", "CURRENT_FIX_END"]].itertuples():
-            fixation = np.asarray(simp[int(rec[1] / stepsize):int(rec[2] / stepsize)])
+            i1,i2 = int(rec[1] * stepsize), int(rec[2] * stepsize)
+            fixation = np.asarray(simp[i1:i2])
+            fixation = fixation[:,:2]
             disps.append(dists(fixation).std())
 
         return disps
@@ -107,15 +110,18 @@ class EyeDat:
         durations = []
         for pat in range(self.pat_l[cond]):
             fix = self.get_df(cond, exp, pat, 'Fix', trial)
-            durations += fix["CURRENT_FIX_DURATION"]
-        return durations
+            durations += [fix["CURRENT_FIX_DURATION"]]
+        return pd.concat(durations)
     
 
 def dists(v):
     length = v.shape[0]
-    sum_x = np.sum(v[:, 0])
-    sum_y = np.sum(v[:, 1])
-    mean = np.array([sum_x/length,sum_y/length])
+    if np.isnan(v).any():
+        return np.asarray(np.nan)
+    # sum_x = np.sum(v[:, 0])
+    # sum_y = np.sum(v[:, 1])
+    # mean = np.array([sum_x/length,sum_y/length])
+    mean = np.nanmean(v)
     deltas = v - mean
     return np.apply_along_axis(lambda x:np.linalg.norm(x), 0, deltas)
 
@@ -126,7 +132,8 @@ def plot(disps,durations,axis):
     X = disps
     y = durations
     D = np.column_stack((X,y))
-    D = D[~(D[:,0]==0)]
+    D = D[~np.isnan(D).any(axis=1)]
+    D = D[~(D==0).any(axis=1)]
 
     axis.scatter(D[:,0],D[:,1], s = 4)
     axis.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
@@ -134,34 +141,51 @@ def plot(disps,durations,axis):
 
     clf = BayesianRidge()
     clf.fit(np.matrix(D[:,0]).transpose(), D[:,1])
-    L=np.linspace(1,max(D[:,0]))
+    L = np.linspace(1,max(D[:,0]))
     axis.plot(L,[clf.predict(x)for x in L],'r')
 
-    return D,clf
+    return D, clf
+
 
 def fname(cond,exp,trial):
     return "./cache/dat_{}_{}_{}".format(cond,exp,trial)
+
+
+def save_object(obj, filename):
+    with open(filename, 'wb') as output:
+        pickle.dump(obj, output, pickle.HIGHEST_PROTOCOL)
+
+
+def load_object(filename):
+    with open(filename, 'rb') as inp:
+        return pickle.load(inp)
+
 
 def main():
     plt.tight_layout
     cache = False
     f=[0,0,0]
-    dat = EyeDat()
-    pickle.dump(dat)
+
+    if os.path.isfile("EyeDump.pcl"):
+        dat = load_object("EyeDump.pcl") # From cache
+    else:
+        dat = EyeDat()
+        save_object(dat,'EyeDump.pcl')
     for trial in range(3):
         f[trial], axarr = plt.subplots(3, 4,  figsize=(16, 12), dpi=160)
         i=0
         for cond in Health:
             for exp in range(1,5):
-                fn = fname(cond,exp,trial)
-                if cache and os.path.isfile(fn + ".npy"):
-                    print("cache\n")
-                    D = np.load(fn + ".npy")
-                    disps, durations = D[:, 0], D[:, 1]
-                else:
-                    disps, durations = dat.disps(cond,exp,trial),dat.durations(cond,exp,trial)
-                D,clf = plot(disps, durations, axarr[i, exp-1])
-                np.save(fn, D)
+                # fn = fname(cond,exp,trial)
+                # if cache and os.path.isfile(fn + ".npy"):
+                #     print("cache\n")
+                #     D = np.load(fn + ".npy")
+                #     disps, durations = D[:, 0], D[:, 1]
+                # else:
+                axarr[i, exp-1].set_ylim(0,2000)
+                disps, durations = dat.disps(cond,exp,trial), dat.durations(cond,exp,trial)
+                D, clf = plot(disps, durations, axarr[i, exp-1])
+                # np.save(fn, D)
             i+=1
         
         plt.savefig("Trial" + str(trial+1))
